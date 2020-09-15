fb7c6d1314b444c608bc059efa9b3d3e5fe59ca3
7amood <mohamedserhan7@gmail.com>
2020-09-15 16:27:02 -0400

    added baby command

---
+++ commands/sounds/baby.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord.js-commando');
+const path = require('path');
+const fileName = path.basename(__filename);
+const cName = fileName.substring(0, fileName.indexOf('.'));
+let dispatcher;
+
+module.exports = class PlayFileCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: cName,
+			group: 'sounds',
+			memberName: cName,
+			description: 'Plays a ' + cName + ' sound file.'
+		});
+	}
+
+	run(message) {
+		function joinChannel() {
+			if (message.member.voice.channel) {
+				if (!message.guild.voiceConnection) {
+					message.member.voice.channel
+						.join()
+						.then((connection) => {
+							dispatcher = connection.play('./SoundFiles/' + cName + 'Sound.mp3');
+							dispatcher.on('finish', (end) => {
+								connection.disconnect();
+							});
+						})
+						.catch(console.error);
+				}
+			} else {
+				message.say('You must be in a voice channel to summon me!');
+			}
+		}
+
+		joinChannel();
+	}
+};
