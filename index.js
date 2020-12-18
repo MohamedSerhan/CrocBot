febc7dc0fa24a65c4cbe0a69d4cc3b0162972ac9
7amood <mohamedserhan7@gmail.com>
2020-12-18 16:41:05 -0500

    register new sounds group

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ const { prefix } = require('./config.json');
 
 const client = new CommandoClient({
 	commandPrefix: '.',
-	owner: '169178004684144650',
+	owner: '169178004684144650'
 });
 
 client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-      ['general', 'General Bot Commands'],
-      ['sounds', 'Sound File Bot Commands'],
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+	.registerDefaultTypes()
+	.registerGroups([
+		[ 'general', 'General Bot Commands' ],
+		[ 'sounds', 'Sound File Bot Commands' ],
+		[ 'new sounds', 'Newly Added Sound Commands' ]
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    client.user.setActivity('Chilling in the water.');
-    client.queue = {};
-    if (typeof client.guilds == 'object' && typeof client.guilds[Symbol.iterator] == 'function') {
-      for (guild of client.guilds) client.queue[guild.id] = [];
-    }
-  });
-    
-  client.on('error', console.error);
+	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity('Chilling in the water.');
+	client.queue = {};
+	if (typeof client.guilds == 'object' && typeof client.guilds[Symbol.iterator] == 'function') {
+		for (guild of client.guilds) client.queue[guild.id] = [];
+	}
+});
+
+client.on('error', console.error);
 
 client.login(process.env.TOKEN);
