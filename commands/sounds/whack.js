b48760149590fa4697fe9ce568bc25015715008e
7amood <mohamedserhan7@gmail.com>
2020-12-21 09:55:28 -0500

    new check for stream in commands

--- commands/sounds/whack.js
+++ commands/sounds/whack.js
@@ -1,7 +1,9 @@
 const { Command } = require('discord.js-commando');
+const fs = require('fs');
 const path = require('path');
 const fileName = path.basename(__filename);
 const cName = fileName.substring(0, fileName.indexOf('.'));
+let botIsStreaming = '';
 let dispatcher;
 
 module.exports = class PlayFileCommand extends Command {
@@ -15,10 +17,18 @@ module.exports = class PlayFileCommand extends Command {
 	}
 
 	run(message) {
+		var streamData = fs.readFileSync('./commands/youtube/stream.txt', { encoding: 'utf8' });
+		if (streamData) {
+			let result = streamData.split(':');
+			botIsStreaming = result[1];
+			console.log(result);
+		}
+		console.log(botIsStreaming);
 		function joinChannel() {
-			if (message.member.voice.channel) {
-				if (!message.guild.voiceConnection) {
-					message.member.voice.channel
+			const voiceChannel = message.member.voice.channel;
+			if (voiceChannel) {
+				if (!message.guild.voiceConnection && botIsStreaming === 'false') {
+					voiceChannel
 						.join()
 						.then((connection) => {
 							dispatcher = connection.play('./SoundFiles/' + cName + 'Sound.mp3');
