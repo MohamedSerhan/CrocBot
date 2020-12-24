9f2195c1201d09ed7b120c48306fb10803e4084c
7amood <mohamedserhan7@gmail.com>
2020-12-24 11:36:05 -0500

    removed restart temporarily

--- commands/general/restart.js
+++ commands/general/restart.js
@@ -13,8 +13,12 @@ module.exports = class StfuCommand extends Command {
 
 	run(message) {
 		message.channel.send('Restarted.').then(() => {
-			fs.writeFileSync('./commands/youtube/stream.txt', 'stream:false');
-			process.exit(1);
+			try {
+				fs.writeFileSync('./commands/youtube/stream.txt', 'stream:false');
+			} catch (error) {
+				console.log('restart: ', error);
+			}
+			//process.exit(1);
 		});
 	}
 };
