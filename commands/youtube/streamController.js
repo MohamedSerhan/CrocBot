4e87c16dedfd2ed21f7942254e3457ebeea95daa
7amood <mohamedserhan7@gmail.com>
2020-12-24 11:29:20 -0500

    add more checks in streamController

--- commands/youtube/streamController.js
+++ commands/youtube/streamController.js
@@ -16,19 +16,23 @@ module.exports = class PlayFileCommand extends Command {
 
 	run(message) {
 		let serverQueue = queue.get(message.guild.id);
-		if (message.content.startsWith('.y play')) {
-			execute(message, serverQueue);
-			return;
-		} else if (message.content.startsWith('.y skip')) {
-			skip(message, serverQueue);
-			return;
-		} else if (message.content.startsWith('.y stop')) {
-			stop(message, serverQueue);
-			return;
-		} else {
-			message.channel.send(
-				'You need to enter a valid command! \n.y play [URL] | Plays specified URL or adds song to queue if there is a song already playing \n.y skip | Skips to next song in queue \n.y stop | Stops all music and clears the queue'
-			);
+		try {
+			if (message.content.startsWith('.y play')) {
+				execute(message, serverQueue);
+				return;
+			} else if (message.content.startsWith('.y skip')) {
+				skip(message, serverQueue);
+				return;
+			} else if (message.content.startsWith('.y stop')) {
+				stop(message, serverQueue);
+				return;
+			} else {
+				message.channel.send(
+					'You need to enter a valid command! \n.y play [URL] | Plays specified URL or adds song to queue if there is a song already playing \n.y skip | Skips to next song in queue \n.y stop | Stops all music and clears the queue'
+				);
+			}
+		} catch (error) {
+			console.log('streamController 1: ', error);
 		}
 
 		async function execute(message, serverQueue) {
@@ -36,89 +40,99 @@ module.exports = class PlayFileCommand extends Command {
 			const voiceChannel = message.member.voice.channel;
 			if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
 			const permissions = voiceChannel.permissionsFor(message.client.user);
-
 			if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
 				return message.channel.send('I need the permissions to join and speak in your voice channel!');
 			}
 
-			const songInfo = await ytdl.getBasicInfo(args[2]);
-			const song = {
-				title: songInfo.videoDetails.title,
-				url: songInfo.videoDetails.video_url
-			};
+			if (args[2].startsWith('https://') || args[2].startsWith('www.')) {
+				const songInfo = await ytdl.getBasicInfo(args[2]);
+				const song = {
+					title: songInfo.videoDetails.title,
+					url: songInfo.videoDetails.video_url
+				};
+				if (!serverQueue) {
+				} else {
+					serverQueue.songs.push(song);
+					return message.channel.send(`${song.title} has been added to the queue!`);
+				}
 
-			if (!serverQueue) {
+				const queueContruct = {
+					textChannel: message.channel,
+					voiceChannel: voiceChannel,
+					connection: null,
+					songs: [],
+					volume: 5,
+					playing: true
+				};
+				// Setting the queue using our contract
+				queue.set(message.guild.id, queueContruct);
+				// Pushing the song to our songs array
+				queueContruct.songs.push(song);
+
+				try {
+					// Here we try to join the voicechat and save our connection into our object.
+					var connection = await voiceChannel.join();
+					queueContruct.connection = connection;
+					// Calling the play function to start a song
+					fs.writeFileSync(__dirname + '/stream.txt', 'stream:true');
+					play(message.guild, queueContruct.songs[0]);
+				} catch (error) {
+					// Printing the error message if the bot fails to join the voicechat
+					console.log('streamController 2: ', error);
+					queue.delete(message.guild.id);
+					return message.channel.send(error);
+				}
 			} else {
-				serverQueue.songs.push(song);
-				//console.log(serverQueue.songs);
-				return message.channel.send(`${song.title} has been added to the queue!`);
-			}
-
-			const queueContruct = {
-				textChannel: message.channel,
-				voiceChannel: voiceChannel,
-				connection: null,
-				songs: [],
-				volume: 5,
-				playing: true
-			};
-			// Setting the queue using our contract
-			queue.set(message.guild.id, queueContruct);
-			// Pushing the song to our songs array
-			queueContruct.songs.push(song);
-			try {
-				// Here we try to join the voicechat and save our connection into our object.
-				var connection = await voiceChannel.join();
-				queueContruct.connection = connection;
-				// Calling the play function to start a song
-				fs.writeFileSync(__dirname + '/stream.txt', 'stream:true');
-				play(message.guild, queueContruct.songs[0]);
-			} catch (err) {
-				// Printing the error message if the bot fails to join the voicechat
-				console.log(err);
-				queue.delete(message.guild.id);
-				return message.channel.send(err);
+				return message.channel.send('Please enter a valid youtube URL! Search by name is not yet implemented!');
 			}
 		}
 
 		function play(guild, song) {
-			const serverQueue = queue.get(guild.id);
-			if (!song) {
-				serverQueue.voiceChannel.leave();
-				queue.delete(guild.id);
-				return;
+			try {
+				const serverQueue = queue.get(guild.id);
+				if (!song) {
+					serverQueue.voiceChannel.leave();
+					queue.delete(guild.id);
+					return;
+				}
+				const dispatcher = serverQueue.connection
+					.play(ytdl(song.url))
+					.on('finish', () => {
+						serverQueue.songs.shift();
+						play(guild, serverQueue.songs[0]);
+					})
+					.on('error', (error) => console.error(error));
+				dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+				serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+			} catch (error) {
+				console.log('streamController 3: ', error);
 			}
-			const dispatcher = serverQueue.connection
-				.play(ytdl(song.url))
-				.on('finish', () => {
-					serverQueue.songs.shift();
-					play(guild, serverQueue.songs[0]);
-				})
-				.on('error', (error) => console.error(error));
-			dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-			serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 		}
 
 		function skip(message, serverQueue) {
-			if (!message.member.voice.channel)
-				return message.channel.send('You have to be in a voice channel to stop the music!');
-			if (!serverQueue) return message.channel.send('There is no song that I could skip!');
-			serverQueue.connection.dispatcher.end();
+			try {
+				if (!message.member.voice.channel)
+					return message.channel.send('You have to be in a voice channel to stop the music!');
+				if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+				serverQueue.connection.dispatcher.end();
+			} catch (error) {
+				console.log('streamController 4: ', error);
+			}
 		}
 
 		function stop(message, serverQueue) {
-			if (!message.member.voice.channel)
-				return message.channel.send('You have to be in a voice channel to stop the music!');
+			try {
+				if (!message.member.voice.channel)
+					return message.channel.send('You have to be in a voice channel to stop the music!');
 
-			if (!serverQueue) return message.channel.send('There is no song that I could stop!');
+				if (!serverQueue) return message.channel.send('There is no song that I could stop!');
 
-			serverQueue.songs = [];
-			serverQueue.connection.dispatcher.end();
-			writeFalse();
-		}
-
-		function writeFalse() {
-			fs.writeFileSync(__dirname + '/stream.txt', 'stream:false');
+				serverQueue.songs = [];
+				fs.writeFileSync(__dirname + '/stream.txt', 'stream:false');
+				serverQueue.connection.dispatcher.end();
+			} catch (error) {
+				console.log('streamController 5: ', error);
+			}
 		}
 	}
 };
